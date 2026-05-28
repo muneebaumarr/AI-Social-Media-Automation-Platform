07339a75d580adf7ce7c8d9baa1769bbe2815abf
muneebaumarr <muneebaomar88@gmail.com>
2026-05-28 16:49:22 +0500

    Add project configuration settings

---
+++ app/config.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+#Database
+
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER') 
+DB_PASSWORD = os.getenv('DB_PASSWORD') 
+
+SERET_KEY = os.getenv('SECRET_KEY')
+ALGORITHM = os.getenv('ALGORITHM', 'HS256')
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES', 60))
+
+#Gemini API
+
+GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
+
+#Email
+EMAIL_ADDRESS = os.getenv('EMAIL_ADDRESS')
+EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
+
+
+
