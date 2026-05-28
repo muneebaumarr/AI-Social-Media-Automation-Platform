d2a1c68defcbe85e49f07eeb403a57379d1a3653
muneebaumarr <muneebaomar88@gmail.com>
2026-05-28 17:09:11 +0500

    Add Project Config Settings

--- app/config.py
+++ app/config.py
@@ -11,7 +11,7 @@ DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER') 
 DB_PASSWORD = os.getenv('DB_PASSWORD') 
 
-SERET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 ALGORITHM = os.getenv('ALGORITHM', 'HS256')
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES', 60))
 
